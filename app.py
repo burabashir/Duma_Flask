11863258fcec299c79c40a981f0c93ea778db414
burabashir <burabashir05@gmail.com>
2026-05-07 15:07:09 +0300

    message code

--- app.py
+++ app.py
@@ -1,195 +1,176 @@
-# import flask
 from flask import *
-
-# importing pymysql
 import pymysql
 import pymysql.cursors
-
-# initialize the flask app
-app = Flask(__name__)
 import os
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
 app.config['UPLOAD_FOLDER'] = 'static/images'
 
-# define the route for corresponding web application function
-@app.route("/api/signup", methods = ['POST'])
 
-# function corresponding to the route
+def get_connection():
+    return pymysql.connect(
+        user='dumabashir',
+        host='mysql-dumabashir.alwaysdata.net',
+        password='modcom1234',
+        database='dumabashir_dumasokogarden',
+        cursorclass=pymysql.cursors.DictCursor
+    )
+
+
+@app.route("/api/signup", methods=['POST'])
 def signup():
-    # getting user inputs
-    username = request.form['username']
-    password = request.form['password']
-    email = request.form['email']
-    phone = request.form['phone']
-    # establishing a connection to our database
-    connection = pymysql.connect (user = 'root', host = 'localhost', password = '', database = 'dumasokogarden')
-    # defining the cursor
-    cursor = connection.cursor()
-    # sql query to insert data into the database
-    sql = "insert into users(username, password, email, phone) values(%s, %s, %s, %s)"
-    # defining the data to replace the placeholders
-    data = (username, password, email, phone)
-    # execute the sql query
-    cursor.execute(sql, data)
-    # commiting changes to database
-    connection.commit()
-    # return response to user
-    return jsonify({"Success": "Thank you for signing up"})
+    username = request.form.get('username')
+    password = request.form.get('password')
+    email = request.form.get('email')
+    phone = request.form.get('phone')
+    role = request.form.get('role')
+
+    if role not in ['student', 'school']:
+        return jsonify({"Error": "Invalid role"}), 400
+
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute(
+        "INSERT INTO users(username, password, email, phone, role) VALUES(%s, %s, %s, %s, %s)",
+        (username, password, email, phone, role)
+    )
+    conn.commit()
+    return jsonify({"Success": "Account created successfully"})
 
 
-
-
-
-
-
-
-
-# define the route for corresponding web application function
-@app.route("/api/signin", methods = ['POST'])
-
-# function corresponding to the route
+@app.route("/api/signin", methods=['POST'])
 def signin():
-    # getting user inputs
-    email = request.form['email']
-    password = request.form['password']
-    # establishing a connection to our database
-    connection = pymysql.connect (user = 'root', host = 'localhost', password = '', database = 'dumasokogarden')
-    # defining the cursor
-    cursor = connection.cursor(pymysql.cursors.DictCursor)
-    # sql query to insert data into the database
-    sql = "select * from users where email = %s and password = %s"
-    # defining the data to replace the placeholders
-    data = ( email,  password)
-    # execute the sql query
-    cursor.execute(sql, data)
-    # check numbers of rows returned
-    count = cursor.rowcount
-    # if condition to check row number
-    if count == 0 :
-        return jsonify ({"Error": "Sign in failed"})
-    else:
-        user = cursor.fetchone()
-        return jsonify ({"Success": "Sign in successful" , "user" : user})
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute(
+        "SELECT * FROM users WHERE email=%s AND password=%s",
+        (email, password)
+    )
+    user = cursor.fetchone()
+
+    if not user:
+        return jsonify({"Error": "Invalid login"}), 401
+
+    return jsonify({
+        "Success": "Login successful",
+        "user": user,
+        "role": user['role']
+    })
 
 
-
-
-# add_product api
-# creating the route
-@app.route("/api/add_product", methods = ['POST'])
-# defining the corresponding web application function
+@app.route("/api/add_product", methods=['POST'])
 def add_product():
-    # get user inputs
-    product_name = request.form['product_name']
-    product_description = request.form['product_description']
-    product_cost = request.form['product_cost']
-    # extracting the imge data
+    role = request.form.get('role')
+    if role != "school":
+        return jsonify({"Error": "Only school can add"}), 403
+
+    name = request.form['product_name']
+    desc = request.form['product_description']
+    cost = request.form['product_cost']
     photo = request.files['product_photo']
-    # extracting file
     filename = photo.filename
-    # defining the photo's file path
-    photo_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-    # saving the filepath
-    photo.save(photo_path)
+    photo.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
-    
-    # establish the connection to the database
-    connection = pymysql.connect (user = 'root', host = 'localhost', password = '', database = 'dumasokogarden')
-    # defining the cursor
-    cursor = connection.cursor()
-    # defining the sql query
-    sql = "insert into product_details(product_name, product_description, product_cost, product_photo) values (%s, %s, %s, %s)"
-    # defining the data
-    data = (product_name, product_description, product_cost, filename)
-    # execute the sql query
-    cursor.execute(sql, data)
-    # commit/save to the database
-    connection.commit()
-    # return a response to the user
-    return jsonify ({"Message": "Product added successfully"})
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute(
+        "INSERT INTO product_details(product_name, product_description, product_cost, product_photo) VALUES(%s, %s, %s, %s)",
+        (name, desc, cost, filename)
+    )
+    conn.commit()
+    return jsonify({"Message": "Talent added"})
 
 
+@app.route("/api/add_clothes", methods=['POST'])
+def add_clothes():
+    role = request.form.get('role')
+    if role != "school":
+        return jsonify({"Error": "Only school can upload clothes"}), 403
+
+    name = request.form['name']
+    desc = request.form['description']
+    price = request.form['price']
+    photo = request.files['photo']
+    filename = photo.filename
+    photo.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute(
+        "INSERT INTO clothes(name, description, price, photo) VALUES(%s, %s, %s, %s)",
+        (name, desc, price, filename)
+    )
+    conn.commit()
+    return jsonify({"Message": "Clothes added"})
 
 
-# get_product_details api
-# definig the route
+@app.route("/api/add_instruments", methods=['POST'])
+def add_instruments():
+    role = request.form.get('role')
+    if role != "school":
+        return jsonify({"Error": "Only school can upload instruments"}), 403
+
+    name = request.form['name']
+    desc = request.form['description']
+    price = request.form['price']
+    photo = request.files['photo']
+    filename = photo.filename
+    photo.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute(
+        "INSERT INTO instruments(name, description, price, photo) VALUES(%s, %s, %s, %s)",
+        (name, desc, price, filename)
+    )
+    conn.commit()
+    return jsonify({"Message": "Instrument added"})
+
+
 @app.route("/api/get_products_details")
-# define the corresponding web application function
-def get_product_details():
-    # establishing connection to database
-    connection = pymysql.connect(user = 'root', host = 'localhost', password = '', database = 'dumasokogarden')
-    # defining the cursor
-    cursor = connection.cursor(pymysql.cursors.DictCursor)
-    # defing the sql query
-    sql = "select * from product_details"
-    # execute the sql query
-    cursor.execute(sql)
-    # fetching all rows retrieved by the sql query
-    product_details = cursor.fetchall()
-    # closing the database connection
-    connection.close()
-    # return a response to the user
-    return jsonify (product_details)
+def get_products():
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM product_details")
+    return jsonify(cursor.fetchall())
 
 
-
-# Mpesa Payment Route/Endpoint 
-import requests
-import datetime
-import base64
-from requests.auth import HTTPBasicAuth
-
-@app.route('/api/mpesa_payment', methods=['POST'])
-def mpesa_payment():
-    if request.method == 'POST':
-        amount = request.form['amount']
-        phone = request.form['phone']
-        # GENERATING THE ACCESS TOKEN
-        # create an account on safaricom daraja
-        consumer_key = "GTWADFxIpUfDoNikNGqq1C3023evM6UH"
-        consumer_secret = "amFbAoUByPV2rM5A"
-
-        api_URL = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"  # AUTH URL
-        r = requests.get(api_URL, auth=HTTPBasicAuth(consumer_key, consumer_secret))
-
-        data = r.json()
-        access_token = "Bearer" + ' ' + data['access_token']
-
-        #  GETTING THE PASSWORD
-        timestamp = datetime.datetime.today().strftime('%Y%m%d%H%M%S')
-        passkey = 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919'
-        business_short_code = "174379"
-        data = business_short_code + passkey + timestamp
-        encoded = base64.b64encode(data.encode())
-        password = encoded.decode('utf-8')
-
-        # BODY OR PAYLOAD
-        payload = {
-            "BusinessShortCode": "174379",
-            "Password": "{}".format(password),
-            "Timestamp": "{}".format(timestamp),
-            "TransactionType": "CustomerPayBillOnline",
-            "Amount": "1",  # use 1 when testing
-            "PartyA": phone,  # change to your number
-            "PartyB": "174379",
-            "PhoneNumber": phone,
-            "CallBackURL": "https://modcom.co.ke/api/confirmation.php",
-            "AccountReference": "account",
-            "TransactionDesc": "account"
-        }
-
-        # POPULAING THE HTTP HEADER
-        headers = {
-            "Authorization": access_token,
-            "Content-Type": "application/json"
-        }
-
-        url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"  # C2B URL
-
-        response = requests.post(url, json=payload, headers=headers)
-        print(response.text)
-        return jsonify({"message": "Please Complete Payment in Your Phone and we will deliver in minutes"})
+@app.route("/api/get_clothes")
+def get_clothes():
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM clothes")
+    return jsonify(cursor.fetchall())
 
 
+@app.route("/api/get_instruments")
+def get_instruments():
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM instruments")
+    return jsonify(cursor.fetchall())
 
-# run the app
-app.run(debug= True)
+
+@app.route("/api/contact", methods=['POST'])
+def contact():
+    email = request.form.get('email')
+    message = request.form.get('message')
+
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute(
+        "INSERT INTO messages(email, message) VALUES(%s, %s)",
+        (email, message)
+    )
+    conn.commit()
+    return jsonify({"Success": "Message received"})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
